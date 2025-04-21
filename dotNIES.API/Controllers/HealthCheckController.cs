3b31c5a186b704986dd04f603049074c50eaeaae
Peter Nies <info@peter-nies.be>
2025-04-21 19:12:22 +0200

    Added healthcheck for API

---
+++ dotNIES.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotNIES.API.Controllers;
+[Route("/")]
+[ApiController]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() => Ok("dotNIES API is running");
+}
