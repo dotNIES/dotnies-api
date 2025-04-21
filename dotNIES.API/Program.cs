5aebe02e4aad51fb6b19ee1a3e27ee8579df5173
Peter Nies <info@peter-nies.be>
2025-04-21 08:24:12 +0200

    Railway setup

--- dotNIES.API/Program.cs
+++ dotNIES.API/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddSwaggerGen(c =>
 });
 
 // Poort ophalen en gebruiken
-var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 var app = builder.Build();
