92d4c0033d133dee7ca85c26a185f187e9734512
Peter Nies <info@peter-nies.be>
2025-04-26 23:27:43 +0200

    Added logging to API

--- dotNIES.API/Controllers/HelloController.cs
+++ dotNIES.API/Controllers/HelloController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CommunityToolkit.Mvvm.Messaging;
+using dotNIES.Data.Logging.Messages;
+using dotNIES.Data.Logging.Models;
+using dotNIES.Data.Logging.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotNIES.API.Controllers;
@@ -6,9 +10,22 @@ namespace dotNIES.API.Controllers;
 [ApiController]
 public class HelloController : ControllerBase
 {
+    private readonly ILoggerService _loggerService;
+
+    public HelloController(ILoggerService loggerService)
+    {
+        _loggerService = loggerService;
+    }
+
     [HttpGet]
     public IActionResult Get()
     {
+        var logMessage = new LogMessageModel
+        {
+            Message = "Hello from dotNIES API",
+            LogLevel = LogLevel.Information
+        };
+        WeakReferenceMessenger.Default.Send(new LogMessage(logMessage));
         return Ok("Hello from dotNIES API");
     }
 }
--- dotNIES.API/Program.cs
+++ dotNIES.API/Program.cs
@@ -1,33 +1,55 @@
+using dotNIES.Data.Logging.Models;
+using dotNIES.Data.Logging.Services;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v2", new OpenApiInfo { Title = "My API", Version = "v2" });
-});
 
-builder.Services.AddHealthChecks();
+// ?? Setup services
+ConfigureServices(builder.Services);
 
-// Poort ophalen en gebruiken
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
-
+// ?? Setup app pipeline
 var app = builder.Build();
+ConfigureMiddleware(app);
 
-app.UseStaticFiles();
-app.UseRouting();
-
-// Zet tijdelijk https redirect UIT tijdens debuggen
-// app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapHealthChecks("/");
-app.MapControllers();
-
-app.UseSwagger();
-app.UseSwaggerUI();
-
+// ?? Run app
 app.Run();
+
+// ------------------
+// Method Definitions
+// ------------------
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddControllers();
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v2", new OpenApiInfo { Title = "My API", Version = "v2" });
+    });
+
+    services.AddHealthChecks();
+
+    services.AddSingleton<IUserLoggerInfoModel, UserLoggerInfoModel>();
+    services.AddSingleton<ILoggerService, LoggerService>();
+}
+
+void ConfigureMiddleware(WebApplication app)
+{
+    // Poort instellen
+    var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+    app.Urls.Add($"http://0.0.0.0:{port}");
+
+    app.UseStaticFiles();
+    app.UseRouting();
+
+    // https redirect eventueel uitzetten tijdens debuggen
+    // app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapHealthChecks("/");
+    app.MapControllers();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
