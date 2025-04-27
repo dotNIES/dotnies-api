ada080d915485dfeb5650fd77ca9524eb23d9baf
Peter Nies <info@peter-nies.be>
2025-04-27 07:45:35 +0200

    fixed swagger problem

--- dotNIES.API/Program.cs
+++ dotNIES.API/Program.cs
@@ -5,7 +5,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v2", new OpenApiInfo { Title = "My API", Version = "v2" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = ".NIES API", Version = "v1" });
 });
 
 builder.Services.AddHealthChecks();
