6b514b05886eeca37a48ed4d119a7d4912a38fa1
Peter Nies <info@peter-nies.be>
2025-04-29 21:01:59 +0200

    WOrk on categorycontroller + start security

---
+++ dotNIES.Data.Dto/Internal/Models/LoginModel.cs
@@ -0,0 +1,6 @@
+﻿namespace dotNIES.Data.Dto.Internal;
+public class LoginModel
+{
+    public string Username { get; set; } = "gebruiker"; // TODO: haal deze weg! string.Empty;
+    public string Password { get; set; } = "wachtwoord"; // TODO: haal deze weg! string.Empty;
+}
