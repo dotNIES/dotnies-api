81b3ecf4601dd7c713c678c775313a13d0ef17da
Peter Nies <info@peter-nies.be>
2025-05-04 17:37:58 +0200

    Work on authentication

--- dotNIES.API/Controllers/AuthController.cs
+++ dotNIES.API/Controllers/AuthController.cs
@@ -1,10 +1,12 @@
 ﻿using dotNIES.Data.Dto.Internal;
 using dotNIES.Data.Logging.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace dotNIES.API.Controllers;
 
@@ -14,22 +16,26 @@ public class AuthController : BaseController
 {
     private readonly IConfiguration _configuration;
     private readonly ILoggerService _loggerService;
+    private readonly UserManager<IdentityUser> _userManager;
 
     public AuthController(IConfiguration configuration,
                           ILoggerService loggerService,
                           IAppInfoDto appInfoDto,
-                          IUserAppInfoDto userAppInfoDto
+                          IUserAppInfoDto userAppInfoDto,
+                          UserManager<IdentityUser> userManager
                          ) : base(loggerService, appInfoDto, userAppInfoDto)
     {
         _configuration = configuration;
         _loggerService = loggerService;
+        _userManager = userManager;
     }
 
-    [HttpPost("login")]
-    public IActionResult Login([FromBody] LoginModel login)
+    [HttpPost("login/{login}")]
+    public async Task<IActionResult> Login(LoginModel login)
     {
-        // TODO: db validatie
-        if (IsValidUser(login))
+        var isValid = await IsValidUser(login);
+
+        if (isValid)
         {
             var token = GenerateJwtToken(login.Username);
 
@@ -43,10 +49,46 @@ public class AuthController : BaseController
         return Unauthorized();
     }
 
-    private bool IsValidUser(LoginModel login)
+    private async Task<bool> IsValidUser(LoginModel login)
     {
         // TODO: db validatie
-        return login.Username == "gebruiker" && login.Password == "wachtwoord";
+        //return login.Username == "gebruiker" && login.Password == "wachtwoord";
+
+        // UserManager is de standaard manier om gebruikers te beheren in ASP.NET Identity
+        var user = await _userManager.FindByNameAsync(login.Username);
+
+        if (user == null)
+        {
+            return false; // Gebruiker niet gevonden
+        }
+
+        // Controleer of het wachtwoord correct is
+        var isPasswordValid = await _userManager.CheckPasswordAsync(user, login.Password);
+
+        if (!isPasswordValid)
+        {
+            return false; // Onjuist wachtwoord
+        }
+
+        // Controleer of de gebruiker is vergrendeld
+        if (user.LockoutEnd != null && user.LockoutEnd > DateTimeOffset.Now)
+        {
+            return false; // Gebruiker is vergrendeld
+        }
+
+        // TODO: require e-mail validatie
+        //// Controleer of e-mail is bevestigd (optioneel)
+        //if (requireEmailConfirmation && !user.EmailConfirmed)
+        //{
+        //    return false; // E-mail is niet bevestigd
+        //}
+
+        // Extra validaties kunnen hier worden toegevoegd
+        // - Controle op specifieke rollen
+        // - Controle op specifieke claims
+        // - Controle op twee-factor authenticatie, etc.
+
+        return true; // Alle validaties zijn geslaagd
     }
 
     private string GenerateJwtToken(string username)
